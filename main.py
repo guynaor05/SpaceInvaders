4d87272a8a0d5f4f22ceaadec525efb7291f3f67
Guy <guynaor05@gmail.com>
2020-12-19 17:36:02 +0200

    fixed movement
    there cant be two key ups

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ bulletY_change = 0.7
 bullet_ready_or_not = "ready"
 font_for_score_and_stage = pygame.font.Font('freesansbold.ttf', 32)
 game_over_font = pygame.font.Font('freesansbold.ttf', 64)
+new_game_font = pygame.font.Font('freesansbold.ttf', 50)
+
 buttonX_new_game = 0
 buttonY_new_game = screen_y - 100
 fontX_score = 10
@@ -50,7 +52,7 @@ distance_for_game_over = math.sqrt((math.pow(ufoX[i] - spaceshipX, 2)) + (math.p
 
 
 def show_font_game_over(x, y):
-    game_over_text_msg = game_over_font.render("Press For New Game", True, (255, 0, 0))
+    game_over_text_msg = new_game_font.render("Press Any key For New Game", True, (255, 0, 0))
     screen.blit(game_over_text_msg, (x, y))
 
 
@@ -97,40 +99,65 @@ def collision_for_game_over(ufoX, ufoY, spaceshipX, spaceshipY):
 
 set_running = True
 running = True
+last_keydown = ''
 while running:
     screen.fill((0, 0, 0))
     screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                # checks that the last key down was left so he wont do two key ups at the same time
+                if last_keydown == 'left':
+                    spaceship_changeX = 0
+                    spaceship_changeY = 0
+            if event.key == pygame.K_RIGHT:
+                # checks that the last key down was right so he wont do two key ups at the same time
+                if last_keydown == 'right':
+                    spaceship_changeX = 0
+                    spaceship_changeY = 0
+            if event.key == pygame.K_DOWN:
+                # checks that the last key down was down so he wont do two key ups at the same time
+                if last_keydown == 'down':
+                    spaceship_changeX = 0
+                    spaceship_changeY = 0
+            if event.key == pygame.K_UP:
+                # checks that the last key down was up so he wont do two key ups at the same time
+                if last_keydown == 'up':
+                    spaceship_changeX = 0
+                    spaceship_changeY = 0
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
+                last_keydown = 'left'
                 spaceship_changeX = -0.5
-            elif event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
+                last_keydown = 'right'
                 spaceship_changeX = 0.5
-            elif event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN:
+                last_keydown = 'down'
                 spaceship_changeY = 0.5
-            elif event.key == pygame.K_UP:
+            if event.key == pygame.K_UP:
+                last_keydown = 'up'
                 spaceship_changeY = -0.5
             if event.key == pygame.K_SPACE:
                 if bullet_ready_or_not == "ready":
                     bulletX = spaceshipX
                     bulletY = spaceshipY
                     fire_bullet(bulletX, bulletY)
-
     for i in range(num_of_ufos):
         if ufoY[i] >= 470:
             for j in range(num_of_ufos):
                 ufoY[j] = 2000
             game_over_text()
             set_running = False
-            break
+
         if collision_for_game_over(ufoX[i], ufoY[i], spaceshipX, spaceshipY):
             for j in range(num_of_ufos):
                 ufoY[j] = 2000
             game_over_text()
             set_running = False
-            break
+
         if ufoX[i] <= 0:
             ufoY[i] += ufo_changeY[i]
             ufo_changeX[i] = 0.2
@@ -174,11 +201,8 @@ while running:
 
         ufoX[i] += ufo_changeX[i]
         ufo(ufoX[i], ufoY[i], i)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP:
-                spaceship_changeX = 0
-                spaceship_changeY = 0
-
+    spaceshipX += spaceship_changeX
+    spaceshipY += spaceship_changeY
     if spaceshipX >= screen_x - spaceship_len:
         spaceshipX = screen_x - spaceship_len
     if spaceshipX <= 0:
@@ -193,8 +217,6 @@ while running:
     if bulletY <= 0:
         bulletY = spaceshipY
         bullet_ready_or_not = "ready"
-    spaceshipX += spaceship_changeX
-    spaceshipY += spaceship_changeY
     spaceship(spaceshipX, spaceshipY)
     show_font_score(fontX_score, fontY_score)
     show_font_stage(fontX_stage, fontY_stage)
@@ -208,33 +230,32 @@ while running:
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                end_running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print(pos)
-                if pos[0] >= 0 and pos[0] <= 650:
-                    if pos[1] >= 500 and pos[1] <= 550:
-                        num_of_ufos = 6
-                        ufoX = []
-                        ufoY = []
-                        ufo_changeX = []
-                        ufo_changeY = []
-                        ufoImg = []
-                        score_value = 0
-                        stage = 1
-                        for _ in range(num_of_ufos):
-                            ufoX.append(random.randint(0, 736))
-                            ufoY.append(random.randint(0, 60))
-                            ufo_changeX.append(0.3)
-                            ufo_changeY.append(60)
-                            ufoImg.append(pygame.image.load('Images/ufo.png'))
+                set_running = True
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if pygame.key.get_pressed():
+                    num_of_ufos = 6
+                    ufoX = []
+                    ufoY = []
+                    ufo_changeX = []
+                    ufo_changeY = []
+                    ufoImg = []
+                    score_value = 0
+                    stage = 1
+                    for _ in range(num_of_ufos):
+                        ufoX.append(random.randint(0, 736))
+                        ufoY.append(random.randint(0, 60))
+                        ufo_changeX.append(0.3)
+                        ufo_changeY.append(60)
+                        ufoImg.append(pygame.image.load('Images/ufo.png'))
 
-                        spaceshipX = 370
-                        spaceshipY = 480
-                        spaceship_changeY = 0
-                        spaceship_changeX = 0
-                        bulletX = 0
-                        bulletY = spaceshipY
-                        bulletX_change = 0
-                        bulletY_change = 0.7
-                        bullet_ready_or_not = "ready"
-                        set_running = True
+                    spaceshipX = 370
+                    spaceshipY = 480
+                    spaceship_changeY = 0
+                    spaceship_changeX = 0
+                    bulletX = 0
+                    bulletY = spaceshipY
+                    bulletX_change = 0
+                    bulletY_change = 0.7
+                    bullet_ready_or_not = "ready"
+                    set_running = True
